14d11f956a34ce3fdddb23154e6b52cc99480fd1
alipileforooshha <alipileforooshha@gmail.com>
2023-01-21 12:13:26 +0330

    add search section

--- src/Rorg.js
+++ src/Rorg.js
@@ -2,62 +2,49 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import OrgChart from 'react-orgchart';
 import 'react-orgchart/index.css';
+import './App.css';
 
 function Rorg() {
+    const[searchWord, setSearchWord] = useState('');
+
     const [state, setState] = useState();
 
     useEffect(() => {
-        
-        fetchUser()
-        return () => {
-            setState();            
-        };
+        console.log('heyyyyy');
+        fetchUser();
     }, []);
     
     const fetchUser = async () => {
-        await axios.get('http://127.0.0.1:8000/api/subs').then(res => {
+        console.log(searchWord)
+        await axios.get(`http://127.0.0.1:8000/api/hierarchy`,{
+            params : {
+                search : searchWord
+            }
+        }).then(res => {
             const result = res.data;
-            setState(result);
+            console.log(res);
+            result !== "error" ? setState(result) : setState();
         })
     }
 
-    const initechOrg = {
-        name: "Bill Lumbergh",
-        actor: "Gary Cole",
-        Role : "Branch",
-        children: [
-            {
-                name: "Peter Gibbons",
-                actor: "Ron Livingston",
-                Role : "Agency",
-                children: [
-                    {
-                        name: "And More!!",
-                        actor: "This is just to show how to build a complex tree with multiple levels of children. Enjoy!",
-                        Role : "Marketer"
-                    }
-                ]
-            },
-            {
-                name: "Milton Waddams",
-                actor: "Stephen Root",
-                Role : "Agency",
-            },
-            {
-                name: "Bob Slydell",
-                actor: "John C. McGi...",
-                Role : "Agency",
-            },
-        ]
-    };
-  
-    return (
-        <div className="App mb-4" id="initechOrgChart">
-            {/* <OrgChart tree={state} NodeComponent={MyNodeComponent} /> */}
-            {state ? <OrgChart tree={state} NodeComponent={MyNodeComponent} /> : ""}
-        </div>
-    )
-}
+        return (
+            <div className="App mb-4 d-flex justify-content-center align-items-center flex-column sticky" id="initechOrgChart">
+                <form className='d-flex flex-column flex-shrink position-sticky sticky-top'>
+                    <input className="form-control my-2" value={searchWord} onChange={(e) => {
+                        setSearchWord(e.target.value);
+                    }}
+                    ></input>
+                    {searchWord}
+                    <button type='submit' className='btn btn-primary my-2' onClick={(e) => {
+                        e.preventDefault();
+                        fetchUser()
+                    }}>جستجو</button>
+                </form>
+                {/* <OrgChart tree={state} NodeComponent={MyNodeComponent} /> */}
+                {state ? <OrgChart tree={state} NodeComponent={MyNodeComponent} /> : <h3>No Results were found</h3>}
+            </div>
+        )
+    }
 
 const MyNodeComponent = ({node}) => {
     const [state,setState] = useState(false);
@@ -69,11 +56,11 @@ const MyNodeComponent = ({node}) => {
             }}
             onMouseLeave={() => {
                 setState(false)
-            }}>{ node.name }
+            }}>{ node.username }
                 <div class="card-body">
-                    <h4 class="card-title">{node.Role}</h4>
-                    <p class="card-text">{node.name}</p>
-                    <a href="#" class="btn btn-primary">See Profile</a>
+                    <h4 class="card-title">{node.email}</h4>
+                    <p class="card-text">{node.username}</p>
+                    <a href="#" class="btn btn-primary rounded-circle border border-primary text-center">+</a>
                 </div>
             </div>
             {state ? 
