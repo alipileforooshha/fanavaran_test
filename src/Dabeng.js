a295b0a7f7bd43f0ae1d90d22b2c2bff83b0247a
alipileforooshha <alipileforooshha@gmail.com>
2023-01-22 15:21:47 +0330

    fix hierarchy tree with specific depth

--- src/Dabeng.js
+++ src/Dabeng.js
@@ -1,69 +1,82 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import ChartContainer from '@dabeng/react-orgchart'
 import axios from 'axios';
 import './App.css';
 import MyNodeComponent from './MyNodeComponent';
 
 function Dabeng() {
-  const[searchWord, setSearchWord] = useState('mr.Admin');
-
+    const[searchWord, setSearchWord] = useState('mr.Admin');
+    
     const [state, setState] = useState();
-
+    const [depth, setDepth] = useState(3);
+    
     useEffect(() => {
-        console.log('heyyyyy');
-        fetchUser();
+        fetchUser()
     }, []);
     
-    const NodeClick = (e) => {
-      console.log(e)
-    }
     const fetchUser = async () => {
-        console.log(searchWord)
         await axios.get(`http://127.0.0.1:8000/api/hierarchy`,{
             params : {
                 search : searchWord
             }
-        }).then(res => {
-            const result = res.data;
-            console.log(res);
-            result !== "error" ? setState(result) : setState();
-            console.log(state);
+        }).then((res) => {
+            // return res.data
+            setState(res.data);
         })
     }
-    // const ds = {
-    //     id: "n1",
-    //     name: "Lao Lao",
-    //     title: "general manager",
-    //     children: [
-    //       { id: "n2", name: "Bo Miao", title: "department manager" },
-    //       {
-    //         id: "n3",
-    //         name: "Su Miao",
-    //         title: "department manager",
-    //         children: [
-    //           { id: "n4", name: "Tie Hua", title: "senior engineer" },
-    //           {
-    //             id: "n5",
-    //             name: "Hei Hei",
-    //             title: "senior engineer",
-    //             children: [
-    //               { id: "n6", name: "Dan Dan", title: "engineer" },
-    //               { id: "n7", name: "Xiang Xiang", title: "engineer" }
-    //             ]
-    //           },
-    //           { id: "n8", name: "Pang Pang", title: "senior engineer" }
-    //         ]
-    //       },
-    //       { id: "n9", name: "Hong Miao", title: "department manager" },
-    //       {
-    //         id: "n10",
-    //         name: "Chun Miao",
-    //         title: "department manager",
-    //         children: [{ id: "n11", name: "Yue Yue", title: "senior engineer" }]
-    //       }
-    //     ]
-    // };
+
+    const NodeClick = (e) => {
+    //   console.log(e)
+    }
+
+    let ds = {
+        id: "n1",
+        username: "Lao Lao",
+        email: "general manager",
+        children: [
+          { id: "n2", username: "Bo Miao", email: "department manager" },
+          {
+            id: "n3",
+            username: "Su Miao",
+            email: "department manager",
+            children: [
+              { id: "n4", username: "Tie Hua", email: "senior engineer" },
+              {
+                id: "n5",
+                username: "Hei Hei",
+                email: "senior engineer",
+                children: [
+                  { id: "n6", username: "Dan Dan", email: "engineer" },
+                  { id: "n7", username: "Xiang Xiang", email: "engineer" }
+                ]
+              },
+              { id: "n8", username: "Pang Pang", email: "senior engineer" }
+            ]
+          },
+          { id: "n9", username: "Hong Miao", email: "department manager" },
+          {
+            id: "n10",
+            username: "Chun Miao",
+            email: "department manager",
+            children: [{ id: "n11", username: "Yue Yue", email: "senior engineer", children: [{ id: "n11", username: "Yue Yue", email: "senior engineer", children: [{ id: "n11", username: "Yue Yue", email: "senior engineer" }] }] }]
+          }
+        ]
+    };
     
+    const getChildren = (node) => {
+        node.children = node?.children?.filter((child) => {
+            if(child.depth < depth){
+                getChildren(child)
+            }
+            else{
+                return
+            }
+            return child.depth === node.depth + 1 && child.depth <= depth;
+        })
+        console.log(node.children)
+        return node
+    }
+
     return (
         <div className="App mb-4 d-flex justify-content-center align-items-center flex-column sticky" id="initechOrgChart">
             <form className='d-flex flex-column flex-shrink position-sticky sticky-top'>
@@ -74,15 +87,16 @@ function Dabeng() {
                 {searchWord}
                 <button type='submit' className='btn btn-primary my-2' onClick={(e) => {
                     e.preventDefault();
-                    fetchUser()
+                    // fetchUser()
                 }}>جستجو</button>
             </form>
-            <ChartContainer datasource={state}
+            {state ? <ChartContainer datasource={getChildren(state)}
             draggable={true}
             onClickNode={(e) => {NodeClick(e)}}
             chartClass="chartClass"
             containerClass="containerClass"
-            NodeTemplate={MyNodeComponent}/>
+            pan={true}
+            NodeTemplate={MyNodeComponent}/> : ""}
         </div>
     )
 }
--- src/MyNodeComponent.js
+++ src/MyNodeComponent.js
@@ -5,41 +5,40 @@ const propTypes = {
   nodeData: PropTypes.object.isRequired
 };
 
-const MyNodeComponent = ({ nodeData }) => {
+const MyNodeComponent = ({ nodeData , depth = 2 }) => {
     const [state,setState] = useState(false);
 
   const selectNode = () => {
     alert("Hi All. I'm " + nodeData.name + ". I'm a " + nodeData.title + ".");
   };
-
-  return (
-    <div className='d-flex flex-row justify-content-center align-items-center'>
-            <div className="initechNode card rounded" 
-            onDoubleClick={() => {
-                setState(!state)
-            }}
-            >{ nodeData.username }
-                <div class="card-body">
-                    <h4 class="card-title">{nodeData.email}</h4>
-                    <p class="card-text">{nodeData.username}</p>
-                    <a href="#" class="btn btn-primary rounded-circle border border-primary text-center">+</a>
-                </div>
-            </div>
-            {state ? 
-            <div class="card-text m-2 p-2 border border-danger rounded">
-                <div>
-                    <span>تعداد زیر مجموعه ها : </span>
-                    <span>{nodeData.children?.length}</span>
-                </div>
-                <div>
-                    <span>آدرس : </span>
-                    <span>{nodeData.Address}</span>
-                </div>
-            </div> 
-            : ""}
-
-        </div>
-  );
+    return (
+      <div className='d-flex flex-row justify-content-center align-items-center'>
+              <div className="initechNode card rounded" 
+              onDoubleClick={() => {
+                  setState(!state)
+              }}
+              >{ nodeData.username }
+                  <div class="card-body">
+                      <h4 class="card-title">{nodeData.email}</h4>
+                      <p class="card-text">{nodeData.username}</p>
+                      <a href="#" class="btn btn-primary rounded-circle border border-primary text-center">+</a>
+                  </div>
+              </div>
+              {state ? 
+              <div class="card-text m-2 p-2 border border-danger rounded">
+                  <div>
+                      <span>تعداد زیر مجموعه ها : </span>
+                      <span>{nodeData.children?.length}</span>
+                  </div>
+                  <div>
+                      <span>آدرس : </span>
+                      <span>{nodeData.Address}</span>
+                  </div>
+              </div> 
+              : ""}
+  
+          </div>
+    );
 };
 
 
