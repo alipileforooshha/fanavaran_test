77e806faac7a33eecc81d67138a87640fc280e9b
alipileforooshha <alipileforooshha@gmail.com>
2023-01-31 14:49:00 +0330

    add login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,24 +2,27 @@ import React, { useEffect, useState } from 'react'
 import {PropTypes} from 'prop-types';
 import axios from 'axios';
 import { async } from 'q';
+import { useNavigate } from 'react-router-dom';
 
 function Login({setToken}) {
     
+    const navigate = useNavigate();
     const [state, setState] = useState({
-        username : '',
+        ssn : '',
         password : ''
     });
 
-    
-    
-
     const postData = async () => {
+        console.log('sending',state)
         return await axios.post('http://127.0.0.1:8000/api/login',{
-            username : state.username,
-            password : state.password
+            ...state
         }).then( res => {
-            setToken(res.data)
-            return res.data;
+            console.log(res)
+            localStorage.setItem('user',JSON.stringify(res.data.user))
+            localStorage.setItem('token',res.data.user.token)
+            navigate('/dashboard');            
+        }).catch(res => {
+            console.log('wwwwwwwww')
         })
     }
 
@@ -32,11 +35,11 @@ function Login({setToken}) {
             <h2 className='text-primary'>Login Form</h2>
             <form className='d-flex flex-column justify-content-center flex-shrink' onSubmit={handleSubmit}>
                 <div className='form-group flex-shrink'>
-                    <label>Username</label>
-                    <input name='username' className='form-control' value={state.username} onChange={(e) => {
+                    <label>ssn</label>
+                    <input name='ssn' className='form-control' value={state.ssn} onChange={(e) => {
                         setState({
                             ...state,
-                            username : e.target.value
+                            ssn : e.target.value
                         })
                     }}></input>
                 </div>
